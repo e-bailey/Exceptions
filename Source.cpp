d2dbec8835d21e6463a82879c9e9bed944e66274
E <e@e.e>
2020-12-06 10:44:29 -0500

    First commit. Created main function driver to test character function.

--- Source.cpp
+++ Source.cpp
@@ -6,18 +6,24 @@ char character(char, int);
 
 int main()
 {
+	char start, value;
+	int offset;
 
 
+	cout << "Enter starting character: ";
+	cin >> start;
 
+	cout << "Enter offset: ";
+	cin >> offset;
+
+
+	value = character(start, offset);
+	cout << value;
 
 
 }
 char character(char start, int offset)
 {
-
-
-
-
 
 
 }
